b5aef1270fd69a2922170b303e51a2f694929dbe
Michael Shinners <mishinners@icloud.com>
2017-12-07 14:22:21 -0800

    errored somewhere.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from imager_profile.views import ProfileView, OtherProfileView, ProfileEditView
 
 urlpatterns = [
-    url(r'$', ProfileView.as_view(), name='my_profile'),
-    url(r'(?P<slug>\w+)$', OtherProfileView.as_view(), name='profile'),
-    url(r'edit/$', ProfileEditView.as_view(), name='profile_edit'),
+    url(r'^$', ProfileView.as_view(), name='my_profile'),
+    url(r'^(?P<slug>\w+)$', OtherProfileView.as_view(), name='profile'),
+    url(r'^edit/$', ProfileEditView.as_view(), name='profile_edit'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,7 +1,7 @@
 """Views for site."""
 from imager_profile.models import ImagerProfile
 from imager_images.models import Album, Photo
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, TemplateView, ListView
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 from random import choice
@@ -33,8 +33,18 @@ class ProfileView(DetailView):
 
     def get_object(self):
         """Get current user profile."""
-        user = self.request.user.profile
-        return user
+        import pdb; pdb.set_trace()
+        self.object = super(ProfileView, self).get_object()
+        profile = self.request.user.profile
+        photos = self.request.user.photos.all()
+        ph_public = len(photos.filter(published="Public"))
+        ph_private = len(photos.filter(published="Private"))
+        albums = self.request.user.albums.all()
+        al_public = len(albums.filter(published="Public"))
+        al_private = len(albums.filter(published="Private"))
+        # return {'user': user, 'ph_public': ph_public, 'ph_private': ph_private,
+        #         'al_public': al_public, 'al_private': al_private}
+        return profile
 
 
 class OtherProfileView(DetailView):
@@ -44,6 +54,12 @@ class OtherProfileView(DetailView):
     model = ImagerProfile
     slug_field = 'user__username'
     context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super(OtherProfileView, self).get_context_data(**kwargs)
+        context['user'] = context['profile'].user
+        return context
+
 
 
 class ProfileEditView(UpdateView):
